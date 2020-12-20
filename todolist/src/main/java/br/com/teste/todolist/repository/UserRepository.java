dfef32d9900d8aed3aca8c8542ba0168e200fbf8
Felipe Pinto <felipe.kanner@gmail.com>
2020-12-20 11:05:19 -0300

    corrigindo bugs

--- todolist/src/main/java/br/com/teste/todolist/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.teste.todolist.repository;
-
-import br.com.teste.todolist.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepository extends JpaRepository<User,Long> {
-}
